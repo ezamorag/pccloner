9dc75bc334feb6f384c16283645cbbc78cdcf74b
Erik Zamora <ezamora1981@gmail.com>
2024-03-26 10:01:17 -0600

    update comments

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='pccloner',
-    version='0.0.10',
+    version='0.0.11',
     description= "Clone your repetitive PC tasks",
     py_modules = ['pccloner.pcdata', 'pccloner.pctask'],
     packages = find_packages(),
